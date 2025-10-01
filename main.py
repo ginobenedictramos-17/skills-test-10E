c72d94852925038fb1e5b2ba49a83b2987a3e7b8
ginobenedictramos-17 <ginobenedict.ramos@gmail.com>
2025-10-02 00:05:34 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyscript import display, document
-#some parts modified by copilot tool to debug
+#some parts of pyscript modified by copilot tool to debug
+#internal css in index.html was made better through copilot suggestions
 def ordering_form(e):
     item1 = document.getElementById("taco1")
     item2 = document.getElementById("taco2")
@@ -7,9 +8,10 @@ def ordering_form(e):
     address = document.getElementById("address").value
     name = document.getElementById("name").value
     number = document.getElementById("number").value
+    mode = document.getElementById("payment").value
 
     total = (float(item1.value) * item1.checked +
              float(item2.value) * item2.checked +   
             float(item3.value) * item3.checked)
 
-    display(f"The total amount is {total} we will contact {name} at {number} when the order has arrived at {address}", target='div1')
+    display(f"The total amount is Php{total}. We will contact {name} at {number} when the order has arrived at {address}, to be paid through {mode}", target='div2')
